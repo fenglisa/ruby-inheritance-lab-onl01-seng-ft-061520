de1c4fb0f4dc915b3e0e4d4ae87d83b7067e888b
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 15:12:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,17 @@
 require_relative "./user.rb"
 class Student  < User
-
+  attr_accessor :knowledge
+  
+  def initialize(knowledge)
+    @knowledge = knowledge
+    knowledge = []
+  end
+  
+  def learn(tidbit)
+    knowledge << tidbit
+  end
+  
+  def knowledge
+    knowledge
+  end
 end
